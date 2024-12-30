50a89ddccc63dec9fe0a03f1bc5908f5948e314a
u229589 <christian.baumberger@sbb.ch>
2024-12-30 14:14:42 +0100

    add simple bigram by counting

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,53 @@
 use std::fs::read_to_string;
+use std::collections::HashMap;
 
 fn main() {
-    let result = read_lines("./makemore/names.txt");
-    let result_part = result[0..10].to_vec();
+    let names = read_lines("./makemore/names.txt");
+    let names_part = names[0..10].to_vec();
 
-    println!("First few names:");
-    println!("{:?}", result_part);
-    println!("Length: {}", result.len());
+    // step-by-step example
+    // println!("First few names:");
+    // println!("{:?}", names_part);
+    // println!("Length: {}", names.len());
+    
+    // let first_name = &names_part[1];
+    // println!("name {:?}", first_name);
+    // let name_chars: Vec<_> = first_name.chars().collect();
+
+    // let char_pairs = name_chars.iter()
+    //     .zip(name_chars.iter().skip(1))
+    //     .collect::<Vec<_>>();
+    // println!("{:?}", char_pairs);
+
+    // let mut bigrams = HashMap::new();
+    // for key in char_pairs.iter() {
+    //     *bigrams.entry(key).or_insert(0) += 1
+    // }
+    // println!("{:?}", bigrams);
+
+    let mut bigram_map = HashMap::new();
+    for name in names_part.iter() {
+    // for name in names.iter() {
+        let name_chars: Vec<_> = name.chars().collect();
+        let char_pairs = name_chars.iter().zip(name_chars.iter().skip(1)).collect::<Vec<_>>();
+        for &(&char_a, &char_b) in char_pairs.iter() {
+            *bigram_map.entry((char_a, char_b)).or_insert(0) += 1;
+        }
+    }
+
+    // order by count value and print
+    let mut count_vec: Vec<_> = bigram_map.iter().collect();
+    count_vec.sort_by(|a, b| b.1.cmp(a.1));
+    for bigram in count_vec.iter() {
+        println!("{:?}", bigram)
+    }
+
 }
 
 fn read_lines(filename: &str) -> Vec<String> {
-    let mut result = Vec::new();
-
-    for line in read_to_string(filename).unwrap().lines() {
-        result.push(line.to_string())
-    }
-
-    result
+    read_to_string(filename)
+        .unwrap() // panic on possible file-reading errors
+        .lines() // split the string into an iterator of string slices
+        .map(String::from) // make each slice into a string
+        .collect() // gather them together into a vector
 }
